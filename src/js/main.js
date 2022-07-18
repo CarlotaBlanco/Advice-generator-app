661203b25857c9692c53aaaa1cc7cde036ebbd75
“CarlotaBlanco” <lotsilots@gmail.com>
2022-07-18 19:01:47 +0200

    add API fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
+const adviceId = document.querySelector('.js-advice-id');
+const advicePhrase = document.querySelector('.js-advice-phrase');
 
-console.log('>> Ready :)');
+function getApiData() {
+  fetch(`https://api.adviceslip.com/advice`)
+    .then((response) => response.json())
+    .then((data) => {
+      adviceId.innerHTML = `Advice # ${data.slip.id}`;
+      advicePhrase.innerHTML = `" ${data.slip.advice}"`;
+    });
+}
+
+getApiData();
